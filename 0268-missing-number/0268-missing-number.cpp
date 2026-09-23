dcdb69829c400b306d93a46eb1e18ab1f1173119
divitakapoor <divitakapoor22@gmail.com>
2026-09-23 20:17:01 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.75%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans^=i;
+        }
+        for(int i:nums){
+            ans^=i;
+        }
+        return ans;
+    }
+};
